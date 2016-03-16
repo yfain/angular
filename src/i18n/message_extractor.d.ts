06efb735e06c06adf2d3364359d70683b7806a41
Miško Hevery <misko@hevery.com>
2016-03-16 23:31:28 +0000

    2f581ff fix(router): RouterOutlet loads component twice in a race condition

--- src/i18n/message_extractor.d.ts
+++ src/i18n/message_extractor.d.ts
@@ -1,7 +1,19 @@
 import { HtmlParser } from 'angular2/src/compiler/html_parser';
 import { ParseSourceSpan, ParseError } from 'angular2/src/compiler/parse_util';
 import { Parser } from 'angular2/src/core/change_detection/parser/parser';
-import { Message } from './message';
+/**
+ * A message extracted from a template.
+ *
+ * The identity of a message is comprised of `content` and `meaning`.
+ *
+ * `description` is additional information provided to the translator.
+ */
+export declare class Message {
+    content: string;
+    meaning: string;
+    description: string;
+    constructor(content: string, meaning: string, description: string);
+}
 /**
  * All messages extracted from a template.
  */
--- ts/src/facade/lang.dart
+++ ts/src/facade/lang.dart
@@ -1,6 +1,6 @@
 library angular.core.facade.lang;
 
-export 'dart:core' show Type, RegExp, print, DateTime, Uri;
+export 'dart:core' show Type, RegExp, print, DateTime;
 import 'dart:math' as math;
 import 'dart:convert' as convert;
 import 'dart:async' show Future, Zone;
@@ -375,8 +375,4 @@ num bitWiseOr(List values) {
 num bitWiseAnd(List values) {
   var val = values.reduce((num a, num b) => (a as int) & (b as int));
   return val as num;
-}
-
-String escape(String s) {
-  return Uri.encodeComponent(s);
 }
