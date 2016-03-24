06af9315de67154b4a4b40baf88a8a53ed5e0cea
Victor Savkin <vic.savkin@gmail.com>
2016-03-24 20:29:10 +0000

    c3fa47b feat(i18n): implement an i18n-aware html parser

--- bundles/angular2-all.umd.js
+++ bundles/angular2-all.umd.js
@@ -1743,6 +1743,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	        regExp.lastIndex = 0;
 	        return { re: regExp, input: input };
 	    };
+	    RegExpWrapper.replaceAll = function (regExp, input, replace) {
+	        var c = regExp.exec(input);
+	        var res = '';
+	        regExp.lastIndex = 0;
+	        var prev = 0;
+	        while (c) {
+	            res += input.substring(prev, c.index);
+	            res += replace(c);
+	            prev = c.index + c[0].length;
+	            regExp.lastIndex = prev;
+	            c = regExp.exec(input);
+	        }
+	        res += input.substring(prev);
+	        return res;
+	    };
 	    return RegExpWrapper;
 	})();
 	exports.RegExpWrapper = RegExpWrapper;
@@ -3818,6 +3833,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return solution;
 	    };
 	    ListWrapper.isImmutable = function (list) { return Object.isSealed(list); };
+	    ListWrapper.flatten = function (array) {
+	        var res = [];
+	        array.forEach(function (a) { return res = res.concat(a); });
+	        return res;
+	    };
 	    return ListWrapper;
 	})();
 	exports.ListWrapper = ListWrapper;
@@ -8608,6 +8628,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	    return ParseException;
 	})(exceptions_1.BaseException);
+	var SplitInterpolation = (function () {
+	    function SplitInterpolation(strings, expressions) {
+	        this.strings = strings;
+	        this.expressions = expressions;
+	    }
+	    return SplitInterpolation;
+	})();
+	exports.SplitInterpolation = SplitInterpolation;
 	var Parser = (function () {
 	    function Parser(/** @internal */ _lexer, providedReflector) {
 	        if (providedReflector === void 0) { providedReflector = null; }
@@ -8659,6 +8687,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return new _ParseAST(input, location, tokens, this._reflector, false).parseTemplateBindings();
 	    };
 	    Parser.prototype.parseInterpolation = function (input, location) {
+	        var split = this.splitInterpolation(input, location);
+	        if (split == null)
+	            return null;
+	        var expressions = [];
+	        for (var i = 0; i < split.expressions.length; ++i) {
+	            var tokens = this._lexer.tokenize(split.expressions[i]);
+	            var ast = new _ParseAST(input, location, tokens, this._reflector, false).parseChain();
+	            expressions.push(ast);
+	        }
+	        return new ast_1.ASTWithSource(new ast_1.Interpolation(split.strings, expressions), input, location);
+	    };
+	    Parser.prototype.splitInterpolation = function (input, location) {
 	        var parts = lang_1.StringWrapper.split(input, INTERPOLATION_REGEXP);
 	        if (parts.length <= 1) {
 	            return null;
@@ -8672,15 +8712,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	                strings.push(part);
 	            }
 	            else if (part.trim().length > 0) {
-	                var tokens = this._lexer.tokenize(part);
-	                var ast = new _ParseAST(input, location, tokens, this._reflector, false).parseChain();
-	                expressions.push(ast);
+	                expressions.push(part);
 	            }
 	            else {
 	                throw new ParseException('Blank expressions are not allowed in interpolated strings', input, "at column " + this._findInterpolationErrorColumn(parts, i) + " in", location);
 	            }
 	        }
-	        return new ast_1.ASTWithSource(new ast_1.Interpolation(strings, expressions), input, location);
+	        return new SplitInterpolation(strings, expressions);
 	    };
 	    Parser.prototype.wrapLiteralPrimitive = function (input, location) {
 	        return new ast_1.ASTWithSource(new ast_1.LiteralPrimitive(input), input, location);
@@ -26367,37 +26405,32 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var directives = this._createDirectiveAsts(element.name, this._parseDirectives(this.selectorMatcher, elementCssSelector), elementOrDirectiveProps, isTemplateElement ? [] : vars, element.sourceSpan);
 	        var elementProps = this._createElementPropertyAsts(element.name, elementOrDirectiveProps, directives);
 	        var children = html_ast_1.htmlVisitAll(preparsedElement.nonBindable ? NON_BINDABLE_VISITOR : this, element.children, Component.create(directives));
-	        // Override the actual selector when the `ngProjectAs` attribute is provided
-	        var projectionSelector = lang_1.isPresent(preparsedElement.projectAs) ?
-	            selector_1.CssSelector.parse(preparsedElement.projectAs)[0] :
-	            elementCssSelector;
-	        var ngContentIndex = component.findNgContentIndex(projectionSelector);
+	        var elementNgContentIndex = hasInlineTemplates ? null : component.findNgContentIndex(elementCssSelector);
 	        var parsedElement;
 	        if (preparsedElement.type === template_preparser_1.PreparsedElementType.NG_CONTENT) {
 	            if (lang_1.isPresent(element.children) && element.children.length > 0) {
 	                this._reportError("<ng-content> element cannot have content. <ng-content> must be immediately followed by </ng-content>", element.sourceSpan);
 	            }
-	            parsedElement = new template_ast_1.NgContentAst(this.ngContentCount++, hasInlineTemplates ? null : ngContentIndex, element.sourceSpan);
+	            parsedElement =
+	                new template_ast_1.NgContentAst(this.ngContentCount++, elementNgContentIndex, element.sourceSpan);
 	        }
 	        else if (isTemplateElement) {
 	            this._assertAllEventsPublishedByDirectives(directives, events);
 	            this._assertNoComponentsNorElementBindingsOnTemplate(directives, elementProps, element.sourceSpan);
-	            parsedElement =
-	                new template_ast_1.EmbeddedTemplateAst(attrs, events, vars, directives, children, hasInlineTemplates ? null : ngContentIndex, element.sourceSpan);
+	            parsedElement = new template_ast_1.EmbeddedTemplateAst(attrs, events, vars, directives, children, elementNgContentIndex, element.sourceSpan);
 	        }
 	        else {
 	            this._assertOnlyOneComponent(directives, element.sourceSpan);
 	            var elementExportAsVars = vars.filter(function (varAst) { return varAst.value.length === 0; });
-	            var ngContentIndex_1 = hasInlineTemplates ? null : component.findNgContentIndex(projectionSelector);
 	            parsedElement =
-	                new template_ast_1.ElementAst(nodeName, attrs, elementProps, events, elementExportAsVars, directives, children, hasInlineTemplates ? null : ngContentIndex_1, element.sourceSpan);
+	                new template_ast_1.ElementAst(nodeName, attrs, elementProps, events, elementExportAsVars, directives, children, elementNgContentIndex, element.sourceSpan);
 	        }
 	        if (hasInlineTemplates) {
 	            var templateCssSelector = createElementCssSelector(TEMPLATE_ELEMENT, templateMatchableAttrs);
 	            var templateDirectives = this._createDirectiveAsts(element.name, this._parseDirectives(this.selectorMatcher, templateCssSelector), templateElementOrDirectiveProps, [], element.sourceSpan);
 	            var templateElementProps = this._createElementPropertyAsts(element.name, templateElementOrDirectiveProps, templateDirectives);
 	            this._assertNoComponentsNorElementBindingsOnTemplate(templateDirectives, templateElementProps, element.sourceSpan);
-	            parsedElement = new template_ast_1.EmbeddedTemplateAst([], [], templateVars, templateDirectives, [parsedElement], ngContentIndex, element.sourceSpan);
+	            parsedElement = new template_ast_1.EmbeddedTemplateAst([], [], templateVars, templateDirectives, [parsedElement], component.findNgContentIndex(templateCssSelector), element.sourceSpan);
 	        }
 	        return parsedElement;
 	    };
@@ -26970,10 +27003,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	            selfClosing = false;
 	        }
 	        var end = this.peek.sourceSpan.start;
-	        var el = new html_ast_1.HtmlElementAst(fullName, attrs, [], new parse_util_1.ParseSourceSpan(startTagToken.sourceSpan.start, end));
+	        var span = new parse_util_1.ParseSourceSpan(startTagToken.sourceSpan.start, end);
+	        var el = new html_ast_1.HtmlElementAst(fullName, attrs, [], span, span, null);
 	        this._pushElement(el);
 	        if (selfClosing) {
 	            this._popElement(fullName);
+	            el.endSourceSpan = span;
 	        }
 	    };
 	    TreeBuilder.prototype._pushElement = function (el) {
@@ -26986,7 +27021,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var tagDef = html_tags_1.getHtmlTagDefinition(el.name);
 	        var parentEl = this._getParentElement();
 	        if (tagDef.requireExtraParent(lang_1.isPresent(parentEl) ? parentEl.name : null)) {
-	            var newParent = new html_ast_1.HtmlElementAst(tagDef.parentToAdd, [], [el], el.sourceSpan);
+	            var newParent = new html_ast_1.HtmlElementAst(tagDef.parentToAdd, [], [el], el.sourceSpan, el.startSourceSpan, el.endSourceSpan);
 	            this._addToParent(newParent);
 	            this.elementStack.push(newParent);
 	            this.elementStack.push(el);
@@ -26998,6 +27033,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    };
 	    TreeBuilder.prototype._consumeEndTag = function (endTagToken) {
 	        var fullName = getElementFullName(endTagToken.parts[0], endTagToken.parts[1], this._getParentElement());
+	        this._getParentElement().endSourceSpan = endTagToken.sourceSpan;
 	        if (html_tags_1.getHtmlTagDefinition(fullName).isVoid) {
 	            this.errors.push(HtmlTreeError.create(fullName, endTagToken.sourceSpan, "Void elements do not have end tags \"" + endTagToken.parts[1] + "\""));
 	        }
@@ -27079,11 +27115,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	})();
 	exports.HtmlAttrAst = HtmlAttrAst;
 	var HtmlElementAst = (function () {
-	    function HtmlElementAst(name, attrs, children, sourceSpan) {
+	    function HtmlElementAst(name, attrs, children, sourceSpan, startSourceSpan, endSourceSpan) {
 	        this.name = name;
 	        this.attrs = attrs;
 	        this.children = children;
 	        this.sourceSpan = sourceSpan;
+	        this.startSourceSpan = startSourceSpan;
+	        this.endSourceSpan = endSourceSpan;
 	    }
 	    HtmlElementAst.prototype.visit = function (visitor, context) { return visitor.visitElement(this, context); };
 	    return HtmlElementAst;
@@ -28194,13 +28232,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	var STYLE_ELEMENT = 'style';
 	var SCRIPT_ELEMENT = 'script';
 	var NG_NON_BINDABLE_ATTR = 'ngNonBindable';
-	var NG_PROJECT_AS = 'ngProjectAs';
 	function preparseElement(ast) {
 	    var selectAttr = null;
 	    var hrefAttr = null;
 	    var relAttr = null;
 	    var nonBindable = false;
-	    var projectAs = null;
 	    ast.attrs.forEach(function (attr) {
 	        var lcAttrName = attr.name.toLowerCase();
 	        if (lcAttrName == NG_CONTENT_SELECT_ATTR) {
@@ -28214,11 +28250,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	        else if (attr.name == NG_NON_BINDABLE_ATTR) {
 	            nonBindable = true;
-	        }
-	        else if (attr.name == NG_PROJECT_AS) {
-	            if (attr.value.length > 0) {
-	                projectAs = attr.value;
-	            }
 	        }
 	    });
 	    selectAttr = normalizeNgContentSelect(selectAttr);
@@ -28236,7 +28267,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    else if (nodeName == LINK_ELEMENT && relAttr == LINK_STYLE_REL_VALUE) {
 	        type = PreparsedElementType.STYLESHEET;
 	    }
-	    return new PreparsedElement(type, selectAttr, hrefAttr, nonBindable, projectAs);
+	    return new PreparsedElement(type, selectAttr, hrefAttr, nonBindable);
 	}
 	exports.preparseElement = preparseElement;
 	(function (PreparsedElementType) {
@@ -28248,12 +28279,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	})(exports.PreparsedElementType || (exports.PreparsedElementType = {}));
 	var PreparsedElementType = exports.PreparsedElementType;
 	var PreparsedElement = (function () {
-	    function PreparsedElement(type, selectAttr, hrefAttr, nonBindable, projectAs) {
+	    function PreparsedElement(type, selectAttr, hrefAttr, nonBindable) {
 	        this.type = type;
 	        this.selectAttr = selectAttr;
 	        this.hrefAttr = hrefAttr;
 	        this.nonBindable = nonBindable;
-	        this.projectAs = projectAs;
 	    }
 	    return PreparsedElement;
 	})();
