d0faeeffc7419fe27be3293d8868762b92539ae9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-04-11 21:47:19 +0000

    ccff175 feat(ngFor): Support convenience  view local in ngFor

--- bundles/angular2-all.umd.js
+++ bundles/angular2-all.umd.js
@@ -21431,7 +21431,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	 *         <pre>{{valueOf(f)}}</pre>
 	 *       </form>
 	 *     </div>
-	 *   `
+	 *   `,
+	 *   directives: [FORM_DIRECTIVES]
 	 * })
 	 * export class App {
 	 *   valueOf(cg: NgControlGroup): string {
