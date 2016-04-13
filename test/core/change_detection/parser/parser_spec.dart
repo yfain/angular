4d20cab89d2b8f1033d84df7e029badeed46c4ca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-04-13 21:58:42 +0000

    45f09ba docs(changelog): update changelog to beta.15

--- test/core/change_detection/parser/parser_spec.dart
+++ test/core/change_detection/parser/parser_spec.dart
@@ -103,6 +103,9 @@ main() {
       it("should parse grouped expressions", () {
         checkAction("(1 + 2) * 3", "1 + 2 * 3");
       });
+      it("should ignore comments in expressions", () {
+        checkAction("a //comment", "a");
+      });
       it("should parse an empty string", () {
         checkAction("");
       });
@@ -266,6 +269,9 @@ main() {
       it("should parse conditional expression", () {
         checkBinding("a < b ? a : b");
       });
+      it("should ignore comments in bindings", () {
+        checkBinding("a //comment", "a");
+      });
     });
     describe("parseTemplateBindings", () {
       keys(List<dynamic> templateBindings) {
@@ -408,6 +414,9 @@ main() {
             '''{{ \'foo\' +
  \'bar\' + \'baz\' }}''',
             '''{{ "foo" + "bar" + "baz" }}''');
+      });
+      it("should ignore comments in interpolation expressions", () {
+        checkInterpolation("{{a //comment}}", "{{ a }}");
       });
     });
     describe("parseSimpleBinding", () {
