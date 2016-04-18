eec0fa5d49bdf1eb0938bccdd5684a34e8f09c7d
Rob Wormald <robwormald@gmail.com>
2016-04-18 20:28:45 +0000

    75463cd chore(perf): return perf metrics from AngularProfiler

--- lib/src/core/application_tokens.dart
+++ lib/src/core/application_tokens.dart
@@ -8,7 +8,7 @@ import "package:angular2/src/facade/lang.dart" show Math, StringWrapper;
  */
 const APP_COMPONENT_REF_PROMISE = const OpaqueToken("Promise<ComponentRef>");
 /**
- * An [OpaqueToken-class.html] representing the application root type in the {@link
+ * An [angular2/di/OpaqueToken] representing the application root type in the {@link
  * Injector}.
  *
  * ```
