9334d37b7eefd8848901f10e1d3d1b6e414b903a
Andrei Alecu <andreialecu@users.noreply.github.com>
2016-04-19 01:00:35 +0000

    566c759 fix(upgrade): clean up scope when element is destroyed

--- es6/prod/src/upgrade/downgrade_ng2_adapter.js
+++ es6/prod/src/upgrade/downgrade_ng2_adapter.js
@@ -142,7 +142,10 @@ export class DowngradeNg2ComponentAdapter {
         }
     }
     registerCleanup() {
-        this.element.bind('$destroy', () => this.viewManager.destroyRootHostView(this.hostViewRef));
+        this.element.bind('$destroy', () => {
+            this.componentScope.$destroy();
+            this.viewManager.destroyRootHostView(this.hostViewRef);
+        });
     }
 }
 class Ng1Change {
